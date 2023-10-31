39363bd178d6c7fa2c1f13d9c554110dba56c1be
SebastianC <sebastian.contreras@btgpactual.com>
2023-10-31 12:47:47 -0500

    Adición de context para pasar los filtros de los fondos

--- parte1/project/administracion-fondos/src/App.jsx
+++ parte1/project/administracion-fondos/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 
 import { products as initialProducts } from "./mocks/products.json"
 
 import { Products } from "./components/Products.jsx"
 import { Header } from "./components/Header"
+import { FiltersContext } from "./context/filters"
 
-function App() {
-  const [products] = useState(initialProducts)
-  const [filters, setFilters] = useState({
-    category: 'all',
-  })
+function useFilters () {
+  const { filters, setFilters } = useContext(FiltersContext)
 
   const filterProducts = (products) => {
     return products.filter(product => {
@@ -20,6 +18,12 @@ function App() {
    })
   }
 
+  return {filterProducts, setFilters}
+}
+
+function App() {
+  const [products] = useState(initialProducts)
+  const {filterProducts, setFilters} = useFilters()
   const filteredProducts = filterProducts(products) 
 
   return (
