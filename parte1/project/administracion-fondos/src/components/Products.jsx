978336e057bb00f0563672b6054709795f868ee0
Sebastián <scontrerasg1999@gmail.com>
2023-11-01 00:39:27 -0500

    Solucionada la parte comparativa entre la plata que tenía disponible y la plata que podía adicionar

--- parte1/project/administracion-fondos/src/components/Products.jsx
+++ parte1/project/administracion-fondos/src/components/Products.jsx
@@ -10,9 +10,12 @@ export function Products ({ products }) {
         const totalInvestments = portfolio.reduce((acc, product) => {
             return acc + product.opening
         }, 0)
+        console.log("Total investments:")
         console.log(totalInvestments)
+        console.log("Product opening:")
+        console.log(product.opening)
         
-        if (product.opening < totalInvestments) {
+        if (product.opening > 500000 - totalInvestments) {
             alert(`No tiene saldo disponible para vincularse al fondo ${product.title}`);
         } else {
             addToPortfolio(product);
