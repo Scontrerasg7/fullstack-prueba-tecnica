1b8c211bbafc0d16ca5d05a371d152553550632b
SebastianC <sebastian.contreras@btgpactual.com>
2023-11-10 18:04:28 -0500

    Adicioné el historial de las transacciones en la API, pero debo hacer que sea conciso ocn lo que hay en el portafolio -> Creo que podría poner el portafolio también en la API y para los productos revisar si están o no en el portafolio

--- parte1/project/administracion-fondos-back/server.py
+++ parte1/project/administracion-fondos-back/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -41,7 +41,24 @@ def get_fondos():
         }       
         ]
     }
+
     return jsonify(fondos)
+
+transactions = []
+@app.route('/transactions', methods=['GET'])
+def get_transactions():
+    return jsonify(transactions)
+
+@app.route('/transactions', methods=['PUT', 'DELETE'])
+def modify_transactions():
+    data = request.get_json()
+
+    if request.method == 'PUT':
+        transactions.insert(0, data)
+    elif request.method == 'DELETE':
+        transactions.remove(data)
+
+    return jsonify({"message": "Transaction modified successfully"})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- parte1/project/administracion-fondos-front/src/context/transactions.jsx
+++ parte1/project/administracion-fondos-front/src/context/transactions.jsx
@@ -1,31 +1,85 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const TransactionsContext = createContext()
 
 export function TransactionsProvider ({ children }) {
     const [transactions, setTransactions] = useState([])
 
-    const newOpening = (product, hash) => {
-        setTransactions(prevState => ([
-            {
+    const apiUrl = 'http://127.0.0.1:5000/transactions'; 
+
+    useEffect(() => {
+        const fetchTransactions = async () => {
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            setTransactions(data);
+        } catch (error) {
+            console.error(error);
+        }
+        };
+
+        fetchTransactions();
+    }, [apiUrl]);
+
+    const newOpening = async (product, hash) => {
+        try {
+            const response = await fetch(apiUrl, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    type: 'Apertura',
+                    product: product,
+                    id: hash,
+                }),
+            });
+            if (!response.ok) {
+                throw new Error(`Failed to add opening transaction: ${response.status}`);
+            }
+
+            setTransactions((prevState) => [
+                {
                 type: 'Apertura',
                 product: product,
-                id: hash
-            },
-            ...prevState
-        ]))        
-    }
+                id: hash,
+                },
+                ...prevState,
+            ]);
+        } catch (error) {
+            console.error(error);
+        }
+        };
 
-    const newCancellation = (product, hash) => {
-        setTransactions(prevState => ([
-            {
-                type: 'Cancelación',
-                product: product,
-                id: hash
-            },
-            ...prevState
-        ]))
-    }
+    const newCancellation = async (product, hash) => {
+        try {
+            const response = await fetch(apiUrl, {
+                    method: "DELETE",
+                    headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    type: 'Cancelación',
+                    product: product,
+                    id: hash,
+                }),
+            });
+            if (!response.ok) {
+                throw new Error(`Failed to add cancellation transaction: ${response.status}`);
+            }
+
+            setTransactions((prevState) => [
+                {
+                    type: 'Cancelación',
+                    product: product,
+                    id: hash,
+                },
+                ...prevState,
+            ]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <TransactionsContext.Provider value={{
